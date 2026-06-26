81f841c0ee272344c5f39c85b5ca3c2fde1198d2
Alcindo Chavarria <139635361+ByOneChav@users.noreply.github.com>
2026-06-26 11:32:38 -0400

    Pipeline-14

--- backend/msvc-inventario/src/main/java/com/microservice/inventario/config/SwaggerConfig.java
+++ backend/msvc-inventario/src/main/java/com/microservice/inventario/config/SwaggerConfig.java
@@ -13,26 +13,29 @@ import io.swagger.v3.oas.models.servers.Server;
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI(){
+	@Bean
+	public OpenAPI customOpenAPI() {
 
-        return new OpenAPI()
+		return new OpenAPI()
 
-                // 🌐 API Gateway como punto de entrada (igual que Pedido)
-                .addServersItem(new Server().url("http://localhost:8080"))
+				// ANTES
+				.addServersItem(new Server().url("http://localhost:8080"))
 
-                // 📌 Información del microservicio
-                .info(
-                        new Info()
-                                .title("Inventario Service - SMARTLOGIX")
-                                .version("v5.6")
-                                .description("""
-                                        Servicio encargado de:
-                                        - Registro de inventario
-                                        - Actualizar inventario
-                                        - Buscar inventario
-                                        - Eliminar inventario
-                                        """)
-                );
-    }
+				// DESPUÉS
+				.addServersItem(new Server().url(
+						"http://a89667c0fa57949db96ac7316657d2ca-1585154752.us-east-1.elb.amazonaws.com"))
+
+				// 📌 Información del microservicio
+				.info(
+						new Info()
+								.title("Inventario Service - SMARTLOGIX")
+								.version("v5.6")
+								.description("""
+										Servicio encargado de:
+										- Registro de inventario
+										- Actualizar inventario
+										- Buscar inventario
+										- Eliminar inventario
+										"""));
+	}
 }
